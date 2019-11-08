4f282e8a5b24acd0dff78aeff57ccee03c130786
Salva San Antonio Cerezo <salsance11@hotmail.com>
2019-11-08 09:08:38 +0100

    Refact: Delete Session class

--- src/main/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/Logic.java
+++ src/main/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/Logic.java
@@ -4,25 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 import es.urjccode.mastercloudapps.adcs.draughts.models.Game;
-import es.urjccode.mastercloudapps.adcs.draughts.models.Session;
 import es.urjccode.mastercloudapps.adcs.draughts.models.State;
 import es.urjccode.mastercloudapps.adcs.draughts.models.StateValue;
 
 public class Logic {
 
-    private Session session;
+	private Game game;
+	private State state;
     private StartController startController;
     private PlayController playController;
     private ResumeController resumeController;
 	private Map<StateValue, Controller> controllers;
 
 	public Logic() {
-		this.session.game = new Game();
-		this.session.state = new State();
+		this.game = new Game();
+		this.state = new State();
         this.controllers = new HashMap<StateValue, Controller>();
-		this.startController = new StartController(this.session);
-		this.playController = new PlayController(this.session);
-		this.resumeController = new ResumeController(this.session);
+		this.startController = new StartController(this.game, this.state);
+		this.playController = new PlayController(this.game, this.state);
+		this.resumeController = new ResumeController(this.game, this.state);
 		this.controllers.put(StateValue.INITIAL, this.startController);
 		this.controllers.put(StateValue.IN_GAME, this.playController);
 		this.controllers.put(StateValue.FINAL, this.resumeController);
@@ -30,7 +30,7 @@ public class Logic {
 	}
 
 	public Controller getController() {
-		return this.controllers.get(this.session.state.getValueState());
+		return this.controllers.get(this.state.getValueState());
     }
 
 }
--- src/test/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/PlayControllerTest.java
+++ src/test/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/PlayControllerTest.java
@@ -7,15 +7,16 @@ import static org.junit.Assert.assertNull;
 import org.junit.Test;
 
 import es.urjccode.mastercloudapps.adcs.draughts.models.Coordinate;
+import es.urjccode.mastercloudapps.adcs.draughts.models.Game;
 import es.urjccode.mastercloudapps.adcs.draughts.models.Piece;
-import es.urjccode.mastercloudapps.adcs.draughts.models.Session;
+import es.urjccode.mastercloudapps.adcs.draughts.models.State;
 import es.urjccode.mastercloudapps.adcs.draughts.models.Color;
 
 public class PlayControllerTest {
 
     @Test
     public void givenPlayControllerWhenMovementRequiereCorrectThenNotError() {
-        PlayController playController = new PlayController(new Session());
+        PlayController playController = new PlayController(new Game(), new State());
         Coordinate origin = new Coordinate(5, 0);
         Coordinate target = new Coordinate(4, 1);
         assertNull(playController.move(origin, target));
