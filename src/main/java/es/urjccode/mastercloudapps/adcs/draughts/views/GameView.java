778b4521b660a3b66fe9ac14b5f3ec5335aaefc1
Salva San Antonio Cerezo <salsance11@hotmail.com>
2019-11-10 11:39:12 +0100

    Refact: Create method writeSquare

--- src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/GameView.java
+++ src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/GameView.java
@@ -12,17 +12,21 @@ public class GameView extends WithConsoleView {
         for(int i=0; i<DIMENSION; i++){
             this.console.write((i+1)+"");
             for(int j=0; j<DIMENSION; j++){
-                Color color = controller.getColor(new Coordinate(i,j));
-                if (color == null){
-                    this.console.write(MessageView.PIECE_COLORS[2]);
-                } else {
-                    this.console.write(MessageView.PIECE_COLORS[color.ordinal()]);
-                }
+                this.writeSquare(controller, new Coordinate(i, j));    
             }
             this.console.writeln((i+1)+"");
         }
         this.writeNumbersLine(DIMENSION);
-	}
+    }
+    
+    private void writeSquare(Controller controller, Coordinate coordinate){
+        Color color = controller.getColor(coordinate);
+        if (color == null){
+            this.console.write(MessageView.PIECE_COLORS[2]);
+        } else {
+            this.console.write(MessageView.PIECE_COLORS[color.ordinal()]);
+        }
+    }
 
     private void writeNumbersLine(final int DIMENSION) {
         this.console.write(" ");
